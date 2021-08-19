2bd48f6062bd8b8b8e707bb192af65d4e06e4a97
Akhil31261 <180031261@kluniversity.in>
2021-08-19 16:43:41 +0530

    Redo4 completed

--- lib/models/types.dart
+++ lib/models/types.dart
@@ -1,18 +1,72 @@
-class ItemModel{
-  static final items = [Item(
-  id: "sport-01",
-  name: "Football",
-  color: "#33505a",
-  image: "https://i.pinimg.com/564x/bc/28/23/bc2823c7ecd8d145b2a5bd3e5ea4a996.jpg"
-)
-];
-  }
+import 'dart:convert';
+
+
+class ItemModel {
+  static List<Item> items=[];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String color;
   final String image;
 
-  Item({required this.id, required this.name,required this.color,required this.image});
+  Item(this.id, this.name, this.color, this.image);
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id ?? this.id,
+      name ?? this.name,
+      color ?? this.color,
+      image ?? this.image,
+    );
   }
 
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'color': color,
+      'image': image,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      map['id'],
+      map['name'],
+      map['color'],
+      map['image'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) => Item.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Item &&
+        other.id == id &&
+        other.name == name &&
+        other.color == color &&
+        other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^ name.hashCode ^ color.hashCode ^ image.hashCode;
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,13 +2,36 @@ import 'package:ardentapp/models/types.dart';
 import 'package:ardentapp/widgets/drawer.dart';
 import 'package:ardentapp/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["catalog_items"];
+    ItemModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(10, (index) => ItemModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -16,14 +39,21 @@ class HomePage extends StatelessWidget {
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: ListView.builder(
-          padding: const EdgeInsets.all(16.0),
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: dummyList[index],
-            );
-          }),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: (ItemModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: ItemModel.items.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(
+                    item: ItemModel.items[index],
+                  );
+                }
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
+      ),
       drawer: MyDrawer(),
     );
   }
